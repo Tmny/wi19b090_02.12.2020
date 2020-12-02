90ed3c8d79c3fd26e4648563fc8239c7002ffce8
Flo <detzer.florian@hotmail.com>
2020-12-02 15:57:09 +0100

    Add project files.

---
+++ wi19b090_02.12.2020/SpecFlowFeature1Steps.cs
@@ -0,0 +1,33 @@
+﻿using TechTalk.SpecFlow;
+using Xunit;
+
+namespace wi19b090_02._12._2020
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(string p0)
+        {
+            Assert.Equal(p0, "Lampe1");
+        }
+
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ wi19b090_02.12.2020/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace wi19b090_02._12._2020
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
